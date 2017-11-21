57f2ac8ed155f80546570cc2bba36982fb973f68
puzanov <puzanov@gmail.com>
2017-11-21 16:20:14 +0600

    added code for max value extraction from json. need to put it in a separate func and refactor

--- main_test.go
+++ main_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"log"
 	"gopkg.in/resty.v1"
+	"encoding/json"
 	sc "github.com/maddevsio/simple-config"
 )
 
@@ -16,6 +17,36 @@ func TestRequest(t *testing.T) {
 
 	log.Print(url)
 	log.Print(resp)
+}
+
+type logData struct {
+	Target string `json:"target"`
+	Datapoints [][]float64 `json:"datapoints"`
+}
+
+func TestParseResult(t *testing.T) {
+	config    := sc.NewSimpleConfig("./config", "yml")
+	url       := config.GetString("url")
+	resp, err := resty.R().Get(url)
+
+	checkErr(err)
+
+	var data []logData
+	_ = json.Unmarshal([]byte(resp.String()), &data)
+
+	var picked []int
+	var max int
+	for _, v := range data[0].Datapoints {
+		log.Print(v[0])
+		if v[0] > 0 {
+			if int(v[0]) > max {
+				max = int(v[0])
+			}
+			picked = append(picked, int(v[0]))
+		}
+	}
+	log.Print(picked)
+	log.Print(max)
 }
 
 func checkErr(err error) {
