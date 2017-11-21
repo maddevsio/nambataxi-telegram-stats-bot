096800af475b5de08822cfaef5ff12cccf2e585e
puzanov <puzanov@gmail.com>
2017-11-21 14:57:37 +0600

    PoC with request

--- main_test.go
+++ main_test.go
@@ -1,7 +1,23 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"log"
+	"gopkg.in/resty.v1"
+	sc "github.com/maddevsio/simple-config"
+)
 
-func Test(t *testing.T) {
-	t.Error("blah")
+func TestRequest(t *testing.T) {
+	config := sc.NewSimpleConfig("./config", "yml")
+	url := config.GetString("url")
+	log.Print(url)
+	resp, err := resty.R().Get(url)
+	checkErr(err)
+	log.Print(resp)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
