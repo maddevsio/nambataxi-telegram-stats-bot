befa73863742a5bc290ca20103e6c816bc3707c5
puzanov <puzanov@gmail.com>
2017-11-22 10:38:44 +0600

    moved funcs to main, tests refactored

--- main.go
+++ main.go
@@ -5,25 +5,51 @@ import (
 	"gopkg.in/telegram-bot-api.v4"
 	"log"
 	"time"
+	"encoding/json"
 )
+
+type logData struct {
+    Target     string      `json:"target"`
+    Datapoints [][]float64 `json:"datapoints"`
+}
 
 func GetDayBeforeInFormat(t time.Time) string {
     return t.AddDate(0, 0, -1).Format("20060102")
 }
 
+func GetMaxDataFromJSON(raw string) int {
+    var data []logData
+    _ = json.Unmarshal([]byte(raw), &data)
+
+    var picked []int
+    var max int
+    for _, v := range data[0].Datapoints {
+        if v[0] > 0 {
+            if int(v[0]) > max {
+                max = int(v[0])
+            }
+            picked = append(picked, int(v[0]))
+        }
+    }
+    return max
+}
+
 func main() {
 	config := sc.NewSimpleConfig("./config", "yml")
-	token := config.GetString("token")
+	token  := config.GetString("token")
 	chatid := int64(config.Get("chatid").(int))
 
 	bot, err := tgbotapi.NewBotAPI(token)
-	if err != nil {
-		log.Panic(err)
-	}
-	bot.Debug = true
-
+	checkErr(err)
 	log.Printf("Authorized on account %s", bot.Self.UserName)
+	bot.Debug = true
 
 	msg := tgbotapi.NewMessage(chatid, "Hello")
 	bot.Send(msg)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
--- main_test.go
+++ main_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"time"
-	"encoding/json"
 	sc "github.com/maddevsio/simple-config"
 	"gopkg.in/resty.v1"
 	"log"
@@ -17,30 +16,8 @@ func TestRequest(t *testing.T) {
 
 	checkErr(err)
 
-	log.Print(url)
-	log.Print(resp)
-}
+	assert.Equal(t, "200 OK", resp.Status())
 
-type logData struct {
-	Target     string      `json:"target"`
-	Datapoints [][]float64 `json:"datapoints"`
-}
-
-func GetMaxDataFromJSON(raw string) int {
-	var data []logData
-	_ = json.Unmarshal([]byte(raw), &data)
-
-	var picked []int
-	var max int
-	for _, v := range data[0].Datapoints {
-		if v[0] > 0 {
-			if int(v[0]) > max {
-				max = int(v[0])
-			}
-			picked = append(picked, int(v[0]))
-		}
-	}
-	return max
 }
 
 func TestParseResult(t *testing.T) {
@@ -57,10 +34,4 @@ func TestGetDataDayBefore(t *testing.T) {
 	timeForTest := time.Date(2017,11,1,0,0,0,0,time.UTC)
 	dayBefore := GetDayBeforeInFormat(timeForTest)
 	assert.Equal(t, "20171031", dayBefore)
-}
-
-func checkErr(err error) {
-	if err != nil {
-		log.Panic(err)
-	}
 }
